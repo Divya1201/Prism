0e5b87ce1d0bc1ab30bd0daf5fb6355c93699c34
Divya1201 <75245503+Divya1201@users.noreply.github.com>
2026-04-10 15:03:57 +0530

    Update request_models.py

--- backend/models/request_models.py
+++ backend/models/request_models.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 
-from typing import Literal
+from typing import Literal, Optional
 from pydantic import BaseModel, Field, HttpUrl
 
 class AnalyzeRequest(BaseModel):
